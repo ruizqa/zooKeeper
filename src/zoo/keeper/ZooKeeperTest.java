b12dc9837d7313bb3a29b97b6b88d857535f4c90
ruizqa <89569188+ruizqa@users.noreply.github.com>
2021-10-18 12:43:14 -0600

    first commit

---
+++ src/zoo/keeper/ZooKeeperTest.java
@@ -0,0 +1,16 @@
+package zoo.keeper;
+
+public class ZooKeeperTest {
+
+	public static void main(String[] args) {
+		Gorilla test = new Gorilla ();
+		test.throwSomething();
+		test.throwSomething();
+		test.throwSomething();
+		test.eatBananas();
+		test.eatBananas();
+		test.climb();
+
+	}
+
+}
